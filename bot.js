c6f9088bf8a2d5e022349779a0dae736d04262fd
roadfor1mcredits1 <44210726+roadfor1mcredits1@users.noreply.github.com>
2018-10-16 20:16:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,9 @@
-const Discord = require('discord.js');                       
-const db = require('quick.db');                                  
-const client = new Discord.Client();                       
-const giphy = require('giphy-api')();                              
-const googl = require('goo.gl');           
-const translate = require('google-translate-api');     
-const fs = require("fs");    
-const getYoutubeID = require('get-youtube-id');                           
-const moment = require("moment");                             
-const { Client, Util } = require('discord.js');              
-const UserBlocked = new Set();            
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const queue = new Map(); 
-const zalgo = require('zalgolize');   
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
- const dateFormat = require('dateformat'); 
- const pretty = require('pretty-ms') 
-,ti={}  
-,spee={};       
-const prefix = '.';
 
-//---------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------
+var ServerID = "501706352234856449"; //اي دي السيرفر
+var ChannelID = "501707152554197013";// اي دي الروم
 
-
-var ServerID = "487639982845067264"; //اي دي السيرفر
-var ChannelID = "487699889535582228";// اي دي الروم
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('warn', console.warn);
 
@@ -57,6 +27,10 @@ function timerFunc() {
 }
 
 var timer = setTimeout(timerFunc, 1000);
+
+
+
+client.login("NTAxNzMxNDQxOTkwODI4MDMy.DqdpwQ.lF7U07VgkeoIJw9RoreEFmx6s3E");
 
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
